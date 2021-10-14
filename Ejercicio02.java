bdca9fcb56c83e8eef4d6780ad428fbbeb453b47
Sergio Domínguez Gómez <91872827+SergioDominguez15@users.noreply.github.com>
2021-10-14 10:49:05 +0200

    Add files via upload

---
+++ Ejercicio02.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ *
+ * @author Sergio D
+ */
+public class Ejercicio02 {
+  public static void main(String[] args) {
+    String Nombre = "Sergio Dominguez Gomez";
+
+    System.out.println("Mi nombre es " + Nombre);
+ 
+  }
+}
---
+++ Ejercicio03.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ *
+ * @author Sergio D
+ */
+public class Ejercicio03 {
+  public static void main(String[] args) {
+    String Nombre = "Sergio Dominguez Gomez";
+    String Direccion = "C/ Jumillano 6 2-Izq";
+    String Telefono = "693463777";
+
+    System.out.println("Mi nombre es " + Nombre); 
+    System.out.println("Direccion es " + Direccion); 
+    System.out.println("Mi telefono es " + Telefono);   
+  }
+}
