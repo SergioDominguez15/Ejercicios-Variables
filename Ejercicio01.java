4c38c73e62ea5475bd09118c7fdac6b265ff6ccb
Sergio Domínguez Gómez <91872827+SergioDominguez15@users.noreply.github.com>
2021-10-14 10:29:42 +0200

    Add files via upload

---
+++ Ejercicio01.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ *
+ * @author Sergio D
+ */
+public class Ejercicio01 {
+  public static void main(String[] args) {
+    int x; // Declara la variable x como entera
+
+    x = 144; // Asigna el valor 5 a la variable x
+    
+    int y;
+    
+    y = 999;
+
+	// Muestra el valor de x
+    System.out.println("El valor actual de mi variable es " + x);
+    System.out.println("El valor actual de mi variable es " + y);
+
+  //Suma
+    System.out.println("Suma de x e y "  + (x + y));
+    
+  //Resta
+    System.out.println("Resta de x e y " + (x - y));
+    
+  //Division
+    System.out.println("Division de x e y " + (x / y));
+    
+  //Multiplicacion
+    System.out.println("Multiplicacion de x e y " + (x * y ));
+    
+    
+
+  }
+}
